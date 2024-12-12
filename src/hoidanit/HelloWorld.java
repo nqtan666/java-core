ebc7aae9ef72a40ff40b47afaf9f9abf83d7a6ba
AnhNLD <anhnld@rikkeisoft.com>
2024-12-12 16:26:25 +0900

    init project

---
+++ src/hoidanit/HelloWorld.java
@@ -0,0 +1,8 @@
+package hoidanit;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("hello world with java hoi dan it");
+		System.out.println("hello world with java hoi dan it");
+	}
+}
